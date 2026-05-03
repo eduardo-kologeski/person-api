d60b75e63ae56e60b30c87daa7575634206acd5d
eduardo-kologeski <eduardokologeski@outlook.com>
2026-05-03 09:49:14 -0300

    Initial commit: adding the first API exercise with Spring Boot

---
+++ personalapi/src/main/java/one/digitalinovation/personalapi/PersonalapiApplication.java
@@ -0,0 +1,14 @@
+package one.digitalinovation.personalapi;
+
+import one.digitalinovation.personalapi.controler.PersonControler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalapiApplication.class, args);
+	}
+
+}
