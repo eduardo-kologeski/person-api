694a0b7cba34c10a5a3c93efcc691accf35a32ae
eduardo-kologeski <eduardokologeski@outlook.com>
2026-05-04 00:01:54 -0300

    feat: Modeling the application domain.

---
+++ personalapi/src/main/java/one/digitalinovation/personalapi/controler/Person.java
@@ -0,0 +1,38 @@
+package one.digitalinovation.personalapi.controler;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST,
+            CascadeType.REMOVE, CascadeType.MERGE})
+
+    private List<Phone> phones;
+}
